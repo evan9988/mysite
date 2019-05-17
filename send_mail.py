1ea869221bcb211acea67fa3c00a048e6374c132
evan <evan@163.com>
2019-05-17 19:56:03 +0800

    去除敏感字符

--- send_mail.py
+++ send_mail.py
@@ -7,7 +7,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 if __name__ == '__main__':
 
-    subject, from_email, to = '来自localhost的测试邮件', 'evan9988@sina.com', '1519301511@qq.com'
+    subject, from_email, to = '来自localhost的测试邮件', '发送方邮箱', '接收方邮箱'
     text_content = '欢迎访问localhost！'
     html_content = '<p>欢迎访问<a href="http://127.0.0.1:8000" target=blank>127.0.0.1:8000</a>，' \
                    '本地登录界面！</p>'
