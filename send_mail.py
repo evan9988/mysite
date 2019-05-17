196daf30013f99c66a5e4e32809cbc15850e2e9c
evan <evan@163.com>
2019-05-17 19:42:02 +0800

    自学

---
+++ send_mail.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import os
+from django.core.mail import EmailMultiAlternatives
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+if __name__ == '__main__':
+
+    subject, from_email, to = '来自localhost的测试邮件', 'evan9988@sina.com', '1519301511@qq.com'
+    text_content = '欢迎访问localhost！'
+    html_content = '<p>欢迎访问<a href="http://127.0.0.1:8000" target=blank>127.0.0.1:8000</a>，' \
+                   '本地登录界面！</p>'
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
